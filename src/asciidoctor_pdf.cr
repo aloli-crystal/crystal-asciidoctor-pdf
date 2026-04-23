8ca4e7c0678ad9a7f47410adaa9ba2e1149fae7d
Philippe Nénert <philippe@aloli.fr>
2026-04-23 07:34:08 +0200

    Synchroniser la constante VERSION avec shard.yml (2.3.24.3)

--- src/asciidoctor_pdf.cr
+++ src/asciidoctor_pdf.cr
@@ -8,7 +8,7 @@ require "./asciidoctor_pdf/syntax_highlighter"
 require "./asciidoctor_pdf/converter"
 
 module AsciidoctorPDF
-  VERSION = "2.3.24"
+  VERSION = "2.3.24.3"
 
   # Version de la gem Ruby asciidoctor-pdf utilisée comme base du portage.
   UPSTREAM_VERSION = "2.3.24"
